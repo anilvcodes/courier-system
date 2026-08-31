6ca6d9bff82167a5f04b69724d8febda21c7bdf6
anilvcodes <anilkumarve51@gmail.com>
2026-08-31 20:32:06 +0530

    change

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
-console.log(" courier-system  ")
+const express = require('express');
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+let parcels=[]
+ app.post("/api/parsel",(req,res)=>{
+
+   const { senderName, receiverName, pickupAddress, deliveryAddress } = req.body;
+
+   if(!senderName|| !receiverName || !pickupAddress || !deliveryAddress){
+    return res.status(400).json({
+      message: "All fields are required"
+    });
+   }
+ const courier={
+  id:parcels.length+1,
+  senderName,
+  receiverName,
+  pickupAddress,
+  deliveryAddress,
+   status: "Pending",
+    createdAt: new Date()
+
+ }
+ parcels.push(courier);
+ 
+  res.status(201).json({
+    message: "Courier created successfully",
+    courier
+  });
+
+ });
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
